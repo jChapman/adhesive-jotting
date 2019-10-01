0e33a6e13fd49fbc145c619042cd8d4535083003
Jordan <jchapman134569@gmail.com>
2019-09-30 23:28:01 -0400

    Cleaned up the look and feel, added delete back

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
-import Draggable from 'react-draggable';
 import './App.css';
-import openSocket from 'socket.io-client'
+//import openSocket from 'socket.io-client'
 
 import Pad from './Components/Pad'
 import Jot from './Components/Jot'
@@ -12,7 +11,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      jots: []
+      jots: [{id: id++, text: 'blah', color: 'rgb(254, 243, 189)'}]
     }
   }
   handleCreateJot = (jotData) => {
@@ -23,6 +22,12 @@ class App extends Component {
     }));
   }
 
+  handleDeleteJot = (jotId) => {
+    this.setState(oldState => ({
+      jots: oldState.jots.filter((jot) => jot.id !== jotId)
+    }))
+  }
+
   render() {
     return (
       <div className="App">
@@ -30,133 +35,13 @@ class App extends Component {
         {this.state.jots.map(jotData => (
           <Jot
             key={jotData.id}
-            color={jotData.color}
-            text={jotData.text}
+            handleDelete={this.handleDeleteJot}
+            {...jotData}
           />
         ))}
       </div>
     );
   }
-}
-
-class OldJot extends Component {
-  constructor(props) {
-    super(props);
-    this.positionUpdate = this.positionUpdate.bind(this);
-    this.state = {
-      id: props.jotData.id,
-      text: props.jotData.text,
-      color: props.jotData.color,
-      position: props.position,
-      positionUpdate: props.positionUpdate,
-      socket: props.socket
-    }
-    this.handleDelete = this.handleDelete.bind(this);
-
-    props.socket.on("jot moved", moveData => {
-      if (moveData.id === this.state.id) {
-        this.setState({position: moveData.position})
-      }
-    });
-  }
-  positionUpdate(e, position) {
-    this.state.positionUpdate(e, position);
-    let {x, y}  = position
-    this.setState({
-      position: { x, y }
-    });
-  }
-
-  handleDelete() {
-    this.props.socket.emit('delete jot', {id: this.state.id});
-  }
-
-  render() {
-    return (
-      <Draggable onStop={this.positionUpdate} position={this.state.position}>
-        <div
-          id={this.state.id}
-          className="box"
-          style={{ background: this.state.color }}
-        >
-          <div style={{textAlign: 'right', paddingRight: '5px'}} onClick={this.handleDelete}>X</div>
-          <p>{this.state.text}</p>
-        </div>
-      </Draggable>
-    );
-  }
-}
-
-const JotContainer = props => (
-  <div className="jot-container">
-    {props.jots.map((jotData) => (
-      <Jot key={jotData.id} jotData={jotData} positionUpdate={props.positionUpdate} position={jotData.position} socket={props.socket}/>
-    ))}
-  </div>
-)
-
-
-class OldApp extends Component {
-  constructor(props) {
-    super(props);
-    this.handleCreateJot = this.handleCreateJot.bind(this);
-    this.handleUpdatePosition = this.handleUpdatePosition.bind(this);
-    this.handleDeleteAll = this.handleDeleteAll.bind(this);
-    this.state = {
-      jots: props.jots,
-      socket: openSocket("http://localhost:8000")
-    }
-    this.state.socket.on('new jot', jotData => {
-      this.setState((oldState) => ({
-        jots: oldState.jots.concat([jotData])
-      }))
-    });
-    this.state.socket.on('delete jot', jotData => {
-      this.setState((oldState) => ({
-        jots: oldState.jots.filter((jot) => {
-          return jot.id !== jotData.id;
-        })
-      }))
-    });
-  }
-
-  handleCreateJot(jotData) {
-    this.state.socket.emit('new jot', jotData);
-  }
-
-  handleUpdatePosition(e, position) {
-    const {x, y} = position;
-    let draggableElement = undefined;
-    for (let element of e.path) {
-      if (element.classList.contains('react-draggable')) {
-        draggableElement = element;
-        break;
-      }
-    }
-    if (!draggableElement)
-      return;
-    this.state.socket.emit('jot moved', {id: parseInt(draggableElement.id), position: {x, y}});
-  }
-
-  handleDeleteAll() {
-    this.state.socket.emit('delete all');
-  }
-
-  /*
-  render() {
-    return (
-      <div className="App">
-        //<JotMaker handleCreateJot={this.handleCreateJot}/>
-        <button onClick={this.handleDeleteAll}>Delete All</button>
-        <JotContainer jots={this.state.jots} positionUpdate={this.handleUpdatePosition} socket={this.state.socket}/>
-      </div>
-    );
-  }
-  */
-}
-
-OldApp.defaultProps = {
-  jots: []
 }
 
 export default App;
--- src/Components/Jot.js
+++ src/Components/Jot.js
@@ -4,7 +4,7 @@ import Draggable from 'react-draggable';
 class Jot extends Component {
   constructor(props) {
     super(props);
-    console.log('Created Jot')
+    console.log("Created Jot");
   }
 
   positionUpdate = (e, position) => {
@@ -15,11 +15,12 @@ class Jot extends Component {
       position: { x, y }
     });
     */
-  }
+  };
 
   handleDelete = () => {
     //this.props.socket.emit('delete jot', {id: this.state.id});
-  }
+    this.props.handleDelete(this.props.id)
+  };
 
   render() {
     return (
@@ -29,11 +30,16 @@ class Jot extends Component {
           className="box"
           style={{ background: this.props.color }}
         >
-          <p>{this.props.text}</p>
+          <div style={{ textAlign: "right"}}>
+            <button id="close-button" onClick={this.handleDelete}>
+              X
+            </button>
+          </div>
+          <p className="jot-text">{this.props.text}</p>
         </div>
       </Draggable>
     );
   }
 }
 
-export default Jot
+export default Jot;
